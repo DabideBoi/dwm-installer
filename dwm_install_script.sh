77f64dd4de9836a14e11ddb4fd64b740693b5336
David Yabis <52210045+DabideBoi@users.noreply.github.com>
2023-02-18 08:45:45 +0800

    optimized script
    used other redditor's suggestions

--- dwm_install_script.sh
+++ dwm_install_script.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
+# Prompt for password at the start
+sudo -n true
+
+# Install dependencies
 sudo dnf install git make gcc libXft-devel libX11-devel libXinerama-devel
-mkdir ~/.suckless
-cd ~/.suckless
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
-git clone https://git.suckless.org/dmenu
-cd dwm
-make
-sudo make clean install
-cd ../st
-make
-sudo make clean install
-cd ../dmenu
-make
-sudo make clean install
-cd
-touch dwm.desktop
+
+# Set up XDG directories
+xdg_config_home=${XDG_CONFIG_HOME:-$HOME/.config}
+xdg_data_home=${XDG_DATA_HOME:-$HOME/.local/share}
+
+mkdir -p "$xdg_data_home/suckless"
+cd "$xdg_data_home/suckless"
+
+# Clone and install dwm, st, and dmenu
+for app in dwm st dmenu; do
+    git clone "https://git.suckless.org/$app"
+    cd "$app"
+    make
+    sudo make clean install
+    cd ..
+done
+
+# Create DWM session file in XDG directory
 echo "Creating DWM session"
 echo "[Desktop Entry]
 Encoding=UTF-8
 Name=DWM
 Exec=dwm-setup
 Icon=dwm
-type=XSession" >> dwm.desktop
-sudo mv dwm.desktop /usr/share/xsessions/dwm.desktop
-echo "Creating dwm-setup config"
-echo "exec dwm" >> dwm-setup
-sudo mv dwm-setup /usr/local/bin/dwm-setup
-chmod +x /usr/local/bin/dwm-setup
-echo "Install now complete. You can now logout and change your DE/WM" 
+Type=XSession" | sudo tee "/usr/share/xsessions/dwm.desktop"
+
+# Create dwm-setup config file in /usr/local/bin
+echo "exec dwm" | sudo tee /usr/local/bin/dwm-setup
+sudo chmod +x /usr/local/bin/dwm-setup
+
+echo "Install now complete. You can now logout and change your DE/WM."
